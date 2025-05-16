9c0f72e47846d8c18e84d128d73cc60af4244cc0
Mecczz07 <me371255@gmail.com>
2025-05-16 15:00:38 -0500

    Agrega scroll suave con clase scroll-smooth para transiciones

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/simulador-crecimiento-logistico/',
   plugins: [
     react(),
     tailwindcss()
